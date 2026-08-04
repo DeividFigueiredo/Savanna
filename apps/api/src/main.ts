d7a514fe15325ce46b260c9f45b12dc1769ad47e
deivid figueiredo <deividborges07@hotmail.com>
2026-08-04 16:20:42 -0300

    correções em main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './config/app.config';
+import * as proccess from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const nomeApp= AppConfig.name
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  await app.listen(proccess.env.APP_PORT || 3000);
 
   console.log(`🚀 ${nomeApp} rodando em http://localhost:3000/api`);
 }
